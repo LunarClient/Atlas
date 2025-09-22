ae7bd923c58e615ea1014dc1e31fa59db4f746fd
jadon fowler <j@jadon.io>
2025-09-22 11:54:13 -0700

    bump to 0.3.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ java {
 
 group = "org.cadixdev"
 project.setProperty("archivesBaseName", project.name.toLowerCase())
-version = "0.3.0"
+version = "0.3.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
